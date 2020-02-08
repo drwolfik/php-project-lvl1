ca92fb8dd4492d3c8b629e2882edaff06aebc321
drwolfik <drwolfik@gmail.com>
2020-02-08 13:06:56 +0300

    corrected language in travis.yml and linter errors

--- src/Cli.php
+++ src/Cli.php
@@ -7,9 +7,9 @@ use function cli\prompt;
 
 function run()
 {
-  line('Welcome to the Brain Games!');
-  $name = prompt('May I have your name?');
-  line("Hello, %s!", $name);
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 
-  return true;
+    return true;
 }
